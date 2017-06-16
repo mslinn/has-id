08a238c45313414d95248e80aee212aca2ae53e4
Mike Slinn <mslinn@micronauticsresearch.com>
2017-06-16 05:49:05 -0700

    WTF

--- src/main/scala/model/persistence/Id.scala
+++ src/main/scala/model/persistence/Id.scala
@@ -9,20 +9,11 @@ protected sealed trait IdType[+T]
 protected object IdType {
   implicit object LongWitness extends IdType[Long]
 
+  implicit object OptionLongWitness extends IdType[Option[Long]]
+
   implicit object StringWitness extends IdType[String]
 
   implicit object UUIDWitness extends IdType[UUID]
-}
-
-trait HasId extends IdImplicitLike {
-  import Id.IdMix
-
-//  def id[U: TypeTag]: Id[_ >: IdMix] = Id.empty[U]
-  def id[T >: IdMix]: Id[_] = Id.empty[T]
-}
-
-trait MaybeHasId[T] extends IdImplicitLike {
-  val id: Option[Id[T]] = None
 }
 
 /** To use, either import `IdImplicits._` or mix in IdImplicitLike */
@@ -31,25 +22,28 @@ trait IdImplicitLike {
 
   implicit class RichStringId(string: String) {
     def toId[A: TypeTag]: Id[_ >: IdMix] = string match {
-      case _ if typeOf[A] <:< typeOf[String] => Id(string)
-      case _ if typeOf[A] <:< typeOf[Long]   => Id(string.toLong)
-      case _ if typeOf[A] <:< typeOf[UUID]   => Id(UUID.fromString(string))
+      case _ if typeOf[A] <:< typeOf[String]       => Id(string)
+      case _ if typeOf[A] <:< typeOf[Long]         => Id(string.toLong)
+      case _ if typeOf[A] <:< typeOf[Option[Long]] => Id(Some(string.toLong))
+      case _ if typeOf[A] <:< typeOf[UUID]         => Id(UUID.fromString(string))
     }
   }
 
   implicit class RichLongId(long: Long) {
     def toId[A: TypeTag]: Id[_ >: IdMix] = long match {
-      case _ if typeOf[A] <:< typeOf[String] => Id(long.toString)
-      case _ if typeOf[A] <:< typeOf[Long]   => Id(long)
-      case _ if typeOf[A] <:< typeOf[UUID]   => Id(UUID.fromString(long.toString))
+      case _ if typeOf[A] <:< typeOf[String]       => Id(long.toString)
+      case _ if typeOf[A] <:< typeOf[Long]         => Id(long)
+      case _ if typeOf[A] <:< typeOf[Option[Long]] => Id(Some(long))
+      case _ if typeOf[A] <:< typeOf[UUID]         => Id(UUID.fromString(long.toString))
     }
   }
 
   implicit class RichIntId(int: Int) {
     def toId[A: TypeTag]: Id[_ >: IdMix] = int match {
-      case _ if typeOf[A] <:< typeOf[String] => Id(int.toString)
-      case _ if typeOf[A] <:< typeOf[Long]   => Id(int.toLong)
-      case _ if typeOf[A] <:< typeOf[UUID]   => Id(UUID.fromString(int.toString))
+      case _ if typeOf[A] <:< typeOf[String]       => Id(int.toString)
+      case _ if typeOf[A] <:< typeOf[Long]         => Id(int.toLong)
+      case _ if typeOf[A] <:< typeOf[Option[Long]] => Id(Some(int.toLong))
+      case _ if typeOf[A] <:< typeOf[UUID]         => Id(UUID.fromString(int.toString))
     }
   }
 }
@@ -57,13 +51,14 @@ trait IdImplicitLike {
 object IdImplicits extends IdImplicitLike
 
 object Id extends IdImplicitLike {
-  type IdMix = String with Long with UUID
+  type IdMix = String with Long with Option[Long] with UUID
 
-  def empty[A: IdType] = "" match {
-      case _ if typeOf[A] <:< typeOf[String] => Id("")
-      case _ if typeOf[A] <:< typeOf[Long]   => Id(0L)
-      case _ if typeOf[A] <:< typeOf[UUID]   => Id(new UUID(0L, 0L))
-    }
+  def empty[A: IdType]: Id[_ >: IdMix] = "" match {
+    case _ if typeOf[A] <:< typeOf[String]       => Id("")
+    case _ if typeOf[A] <:< typeOf[Long]         => Id(0L)
+    case _ if typeOf[A] <:< typeOf[Option[Long]] => Id(None)
+    case _ if typeOf[A] <:< typeOf[UUID]         => Id(new UUID(0L, 0L))
+  }
 
   def isValid[T: IdType](value: T): Boolean = try {
     Id(value)
@@ -74,11 +69,19 @@ object Id extends IdImplicitLike {
 }
 
 case class Id[T: IdType](value: T) extends HasValue[T] {
-  def isZero[A: TypeTag]: Boolean = value match {
-    case _ if typeOf[A] <:< typeOf[String] => value == Id.empty[String]
-    case _ if typeOf[A] <:< typeOf[Long]   => value == Id.empty[Long]
-    case _ if typeOf[A] <:< typeOf[UUID]   => value == Id.empty[UUID]
+  def isEmpty[A: TypeTag]: Boolean = value match {
+    case _ if typeOf[A] <:< typeOf[String]       => value == Id.empty[String]
+    case _ if typeOf[A] <:< typeOf[Long]         => value == Id.empty[Long]
+    case _ if typeOf[A] <:< typeOf[Option[Long]] => value == Id.empty[Option[Long]]
+    case _ if typeOf[A] <:< typeOf[UUID]         => value == Id.empty[UUID]
   }
 
   override def toString: String = value.toString
+}
+
+trait HasId extends IdImplicitLike {
+  import Id.IdMix
+
+//  def id[U: TypeTag]: Id[_ >: IdMix] = Id.empty[U]
+  def id[T >: IdMix]: Id[_] = Id.empty[T]
 }
--- src/test/scala/IdTest.scala
+++ src/test/scala/IdTest.scala
@@ -12,6 +12,7 @@ class IdTest extends WordSpec with MustMatchers {
   "Ids" should {
     "provide zero values" in {
       Id.empty[Long].value mustBe 0L
+      Id.empty[Option[Long]].value mustBe None
       Id.empty[String].value mustBe ""
 
       val desired: String = new UUID(0L, 0L).toString
@@ -27,17 +28,22 @@ class IdTest extends WordSpec with MustMatchers {
       blah.id mustBe id
     }
 
-    "compare to zero" in {
+    "compare to empty / zero" in {
       Id.empty[UUID] mustBe Id(new UUID(0L, 0L))
       Id.empty[String] mustBe Id("")
       Id.empty[Long] mustBe Id(0L)
+      Id.empty[Option[Long]] mustBe Id(None)
 
-      case class BlahString(a: Int, b: String, override val id: Id[String] = Id.empty[String]) extends HasId
-      case class BlahLong(a: Int,   b: String, override val id: Id[Long]   = Id.empty[Long])   extends HasId
-      case class BlahUuid(a: Int,   b: String, override val id: Id[UUID]   = Id.empty[UUID])   extends HasId
+      case class BlahString(a: Int,     b: String, override val id: Id[String]      = Id.empty) extends HasId
+      case class BlahLong(a: Int,       b: String, override val id: Id[Long]        = Id.empty) extends HasId
+      case class BlahOptionLong(a: Int, b: String, override val id: Id[Option[Long] = Id.empty) extends HasId
+      case class BlahUuid(a: Int,       b: String, override val id: Id[UUID]        = Id.empty) extends HasId
 
       val idLongZero: Id[_ >: IdMix] = Id.empty[Long]
       idLongZero mustBe Id(0L)
+
+      val idOptionLongZero: Id[_ >: IdMix] = Id.empty[Option[Long]]
+      idOptionLongZero mustBe Id(None)
 
       val idStringZero: Id[_ >: IdMix] = Id.empty[String]
       idStringZero mustBe Id("")
@@ -50,6 +56,21 @@ class IdTest extends WordSpec with MustMatchers {
       Id.isValid[UUID](UUID.randomUUID)
       Id.isValid[String]("hello")
       Id.isValid[Long](42L)
+      Id.isValid[Option[Long]](Some(42L))
+    }
+
+    "pass empty / zero test 1" in {
+      Id.isEmpty(Id.empty[UUID])
+      Id.isEmpty(Id.empty[String])
+      Id.isEmpty(Id.empty[Long])
+      Id.isEmpty(Id.empty[[Option[Long]]]))
+    }
+
+    "pass empty / zero test 2" in {
+      Id.isEmpty(Id(new UUID(0L, 0L)))
+      Id.isEmpty(Id(""))
+      Id.isEmpty(Id(0L))
+      Id.isEmpty(Id[Option[Long]](None))
     }
   }
 }

dce0e0521d2eae9e29dcd5cdda4ed74a1e9f2626
Mike Slinn <mslinn@micronauticsresearch.com>
2017-06-16 05:01:20 -0700

    WTF

--- src/main/scala/model/persistence/Id.scala
+++ src/main/scala/model/persistence/Id.scala
@@ -4,26 +4,21 @@ import java.util.UUID
 import com.micronautics.HasValue
 import scala.reflect.runtime.universe._
 
-protected sealed trait IdType[T] {
-  def empty: Id[T]
-}
+protected sealed trait IdType[+T]
 
 protected object IdType {
-  implicit object LongWitness extends IdType[Long] {
-    val empty: Id[Long] = Id(0L)
-  }
+  implicit object LongWitness extends IdType[Long]
 
-  implicit object StringWitness extends IdType[String] {
-    val empty: Id[String] = Id("")
-  }
+  implicit object StringWitness extends IdType[String]
 
-  implicit object UUIDWitness extends IdType[UUID] {
-    val empty: Id[UUID] = Id(new UUID(0L, 0L))
-  }
+  implicit object UUIDWitness extends IdType[UUID]
 }
 
 trait HasId extends IdImplicitLike {
-  def id[T: IdType]: Id[_ >: IdMix] = Id.empty
+  import Id.IdMix
+
+//  def id[U: TypeTag]: Id[_ >: IdMix] = Id.empty[U]
+  def id[T >: IdMix]: Id[_] = Id.empty[T]
 }
 
 trait MaybeHasId[T] extends IdImplicitLike {
@@ -32,7 +27,7 @@ trait MaybeHasId[T] extends IdImplicitLike {
 
 /** To use, either import `IdImplicits._` or mix in IdImplicitLike */
 trait IdImplicitLike {
-  type IdMix = String with Long with UUID
+  import Id.IdMix
 
   implicit class RichStringId(string: String) {
     def toId[A: TypeTag]: Id[_ >: IdMix] = string match {
@@ -62,11 +57,13 @@ trait IdImplicitLike {
 object IdImplicits extends IdImplicitLike
 
 object Id extends IdImplicitLike {
-  def empty[A: TypeTag]: Id[_ >: IdMix] = "" match {
-    case _ if typeOf[A] <:< typeOf[String] => IdType.StringWitness.empty
-    case _ if typeOf[A] <:< typeOf[Long]   => IdType.LongWitness.empty
-    case _ if typeOf[A] <:< typeOf[UUID]   => IdType.UUIDWitness.empty
-  }
+  type IdMix = String with Long with UUID
+
+  def empty[A: IdType] = "" match {
+      case _ if typeOf[A] <:< typeOf[String] => Id("")
+      case _ if typeOf[A] <:< typeOf[Long]   => Id(0L)
+      case _ if typeOf[A] <:< typeOf[UUID]   => Id(new UUID(0L, 0L))
+    }
 
   def isValid[T: IdType](value: T): Boolean = try {
     Id(value)
@@ -77,5 +74,11 @@ object Id extends IdImplicitLike {
 }
 
 case class Id[T: IdType](value: T) extends HasValue[T] {
+  def isZero[A: TypeTag]: Boolean = value match {
+    case _ if typeOf[A] <:< typeOf[String] => value == Id.empty[String]
+    case _ if typeOf[A] <:< typeOf[Long]   => value == Id.empty[Long]
+    case _ if typeOf[A] <:< typeOf[UUID]   => value == Id.empty[UUID]
+  }
+
   override def toString: String = value.toString
 }
--- src/test/scala/IdTest.scala
+++ src/test/scala/IdTest.scala
@@ -1,9 +1,11 @@
 import java.util.UUID
-import model.persistence.Id
+import model.persistence.{HasId, Id}
+import model.persistence.Id.IdMix
 import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
-import org.scalatest._
 import org.scalatest.Matchers._
+import org.scalatest._
+import org.scalatest.junit.JUnitRunner
+import scala.language.existentials
 
 @RunWith(classOf[JUnitRunner])
 class IdTest extends WordSpec with MustMatchers {
@@ -17,12 +19,31 @@ class IdTest extends WordSpec with MustMatchers {
       actual mustBe desired
     }
 
-    "compare" in {
+    "compare to a value" in {
       case class Blah(a: Int, b: String, id: Id[UUID]=Id(UUID.randomUUID))
 
       val id = Id(UUID.randomUUID)
       val blah = Blah(1, "two", id)
       blah.id mustBe id
+    }
+
+    "compare to zero" in {
+      Id.empty[UUID] mustBe Id(new UUID(0L, 0L))
+      Id.empty[String] mustBe Id("")
+      Id.empty[Long] mustBe Id(0L)
+
+      case class BlahString(a: Int, b: String, override val id: Id[String] = Id.empty[String]) extends HasId
+      case class BlahLong(a: Int,   b: String, override val id: Id[Long]   = Id.empty[Long])   extends HasId
+      case class BlahUuid(a: Int,   b: String, override val id: Id[UUID]   = Id.empty[UUID])   extends HasId
+
+      val idLongZero: Id[_ >: IdMix] = Id.empty[Long]
+      idLongZero mustBe Id(0L)
+
+      val idStringZero: Id[_ >: IdMix] = Id.empty[String]
+      idStringZero mustBe Id("")
+
+      val idUuidZero: Id[_ >: IdMix] = Id.empty[UUID]
+      idUuidZero mustBe Id(new UUID(0L, 0L))
     }
 
     "pass validity test" in {
